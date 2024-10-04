3fc1a8899e9ac5e43c970fd63ee17f16afa17f4b
kristal1ik <korobovtsevaolga@yandex.ru>
2024-10-04 14:12:16 +0300

    testing the Object class from Java

---
+++ app/src/main/java/com/kristallik/jokeapp/Test.kt
@@ -0,0 +1,24 @@
+package com.kristallik.jokeapp
+
+import java.util.Objects
+
+
+fun main(){
+    val n: Int = 5;
+    val arr: Array<Int> = arrayOf(1, 2, 3)
+    val lst: List<Double> = listOf(1.7, 2.7, 3.9)
+    println(n)
+    objCheck(Object())
+}
+fun intCheck(intElem: Int){
+
+}
+fun arrCheck(arrElem: Array<Int>){
+
+}
+fun listCheck(listElem: List<Int>){
+
+}
+fun objCheck(objElem: Object){
+    println(objElem)
+}
