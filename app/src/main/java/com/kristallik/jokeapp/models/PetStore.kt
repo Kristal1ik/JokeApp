ace3bbed92da3f6522656f5466090659a0df5e97
kristal1ik <korobovtsevaolga@yandex.ru>
2024-10-23 23:23:17 +0300

    refactor(PetStore.kt)

--- app/src/main/java/com/kristallik/jokeapp/models/PetStore.kt
+++ app/src/main/java/com/kristallik/jokeapp/models/PetStore.kt
@@ -1,12 +1,14 @@
 package com.kristallik.jokeapp.models
 
 import com.kristallik.jokeapp.interfaces.Animal
+import com.kristallik.jokeapp.interfaces.Cat
+import com.kristallik.jokeapp.interfaces.Dog
 
 class PetStore {
     fun identifyAnimal(animal: Animal?): String? {
         return when (animal) {
-            is DogHusky, is DogCorgi -> "It's a DOG\n\"U・ᴥ・U\""
-            is CatScottish, is CatSiamese -> "It's a CAT\n\"/ᐠ.｡.ᐟ\\\\ᵐᵉᵒʷˎˊ˗\""
+            is Dog -> "It's a DOG\n\"U・ᴥ・U\""
+            is Cat -> "It's a CAT\n\"/ᐠ.｡.ᐟ\\\\ᵐᵉᵒʷˎˊ˗\""
             else -> null
         }
     }
