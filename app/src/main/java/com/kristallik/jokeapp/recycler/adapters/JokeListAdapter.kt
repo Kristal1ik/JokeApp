d7ba000e5c7d86fa02168793d4559f3529d7e004
kristal1ik <korobovtsevaolga@yandex.ru>
2024-11-04 23:08:32 +0300

    feat(JokeGenerator.kt, JokeListAdapter.kt): some changes for Intent class

--- app/src/main/java/com/kristallik/jokeapp/recycler/adapters/JokeListAdapter.kt
+++ app/src/main/java/com/kristallik/jokeapp/recycler/adapters/JokeListAdapter.kt
@@ -3,12 +3,13 @@ package com.kristallik.jokeapp.recycler.adapters
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.kristallik.jokeapp.data.Joke
 import com.kristallik.jokeapp.databinding.JokeItemBinding
 import com.kristallik.jokeapp.recycler.JokeViewHolder
 import com.kristallik.jokeapp.recycler.util.JokeItemCallback
 
-class JokeListAdapter :
+class JokeListAdapter(private val clickListener: (Int) -> Unit):
     ListAdapter<Joke, JokeViewHolder>(JokeItemCallback()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JokeViewHolder {
         val binding = JokeItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -17,7 +18,20 @@ class JokeListAdapter :
 
     override fun onBindViewHolder(holder: JokeViewHolder, position: Int) {
         holder.bind(currentList[position])
+        holder.itemView.setOnClickListener{
+            handleJokeClick(position)
+        }
     }
 
     override fun getItemCount(): Int = currentList.size
-}
+
+    private fun handleJokeClick(position: Int) {
+        if (position != RecyclerView.NO_POSITION) {
+            currentList[position]?.let { joke ->
+                clickListener(position)
+            }
+        }
+    }
+
+
+    }
