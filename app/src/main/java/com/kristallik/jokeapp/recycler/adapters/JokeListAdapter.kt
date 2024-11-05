38479474204ea4c671b4d6587b1aed9c6ef27e73
kristal1ik <korobovtsevaolga@yandex.ru>
2024-11-05 23:08:36 +0300

    feat(JokeListAdapter.kt): callback

--- app/src/main/java/com/kristallik/jokeapp/recycler/adapters/JokeListAdapter.kt
+++ app/src/main/java/com/kristallik/jokeapp/recycler/adapters/JokeListAdapter.kt
@@ -3,13 +3,12 @@ package com.kristallik.jokeapp.recycler.adapters
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import com.kristallik.jokeapp.data.Joke
 import com.kristallik.jokeapp.databinding.JokeItemBinding
 import com.kristallik.jokeapp.recycler.JokeViewHolder
 import com.kristallik.jokeapp.recycler.util.JokeItemCallback
 
-class JokeListAdapter(private val clickListener: (Int) -> Unit):
+class JokeListAdapter(private val clickListener: (Int) -> Unit) :
     ListAdapter<Joke, JokeViewHolder>(JokeItemCallback()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JokeViewHolder {
         val binding = JokeItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -18,20 +17,10 @@ class JokeListAdapter(private val clickListener: (Int) -> Unit):
 
     override fun onBindViewHolder(holder: JokeViewHolder, position: Int) {
         holder.bind(currentList[position])
-        holder.itemView.setOnClickListener{
-            handleJokeClick(position)
+        holder.itemView.setOnClickListener {
+            clickListener(position)
         }
     }
 
     override fun getItemCount(): Int = currentList.size
-
-    private fun handleJokeClick(position: Int) {
-        if (position != RecyclerView.NO_POSITION) {
-            currentList[position]?.let { joke ->
-                clickListener(position)
-            }
-        }
-    }
-
-
-    }
+}
