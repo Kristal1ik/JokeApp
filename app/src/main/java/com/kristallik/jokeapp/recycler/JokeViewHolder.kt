3f788e3247dc638cab7fa75d9b3b1e3a825f103d
kristal1ik <korobovtsevaolga@yandex.ru>
2024-11-24 17:17:52 +0300

    feat(JokeListAdapter.kt, JokeViewHolder.kt): new logic for some different jokes ui

--- app/src/main/java/com/kristallik/jokeapp/recycler/JokeViewHolder.kt
+++ app/src/main/java/com/kristallik/jokeapp/recycler/JokeViewHolder.kt
@@ -1,17 +1,28 @@
 package com.kristallik.jokeapp.recycler
 
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.kristallik.jokeapp.data.Joke
 import com.kristallik.jokeapp.databinding.JokeItemBinding
+import com.kristallik.jokeapp.databinding.JokeItemNetBinding
 
-// Cлужит своеобразным контейнером для всех компонентов, которые входят в элемент списка. Держит в памяти ссылку на конкретный элемент списка (view)
+// Служит своеобразным контейнером для всех компонентов, которые входят в элемент списка. Держит в памяти ссылку на конкретный элемент списка (view)
 // Переиспользует для новых значений
-class JokeViewHolder(
-    private val binding: JokeItemBinding
-) : RecyclerView.ViewHolder(binding.root) {
-    fun bind(joke: Joke) {
-        binding.category.text = joke.category
-        binding.answer.text = joke.answer
-        binding.question.text = joke.question
+sealed class JokeViewHolder(binding: View) : RecyclerView.ViewHolder(binding) {
+    abstract fun bind(joke: Joke)
+    class NetworkJokeViewHolder(private val binding: JokeItemNetBinding) :
+        JokeViewHolder(binding.root) {
+        override fun bind(joke: Joke) {
+            binding.question.text = joke.setup
+            binding.answer.text = joke.delivery
+        }
+    }
+
+    class ManualJokeViewHolder(private val binding: JokeItemBinding) :
+        JokeViewHolder(binding.root) {
+        override fun bind(joke: Joke) {
+            binding.question.text = joke.setup
+            binding.answer.text = joke.delivery
+        }
     }
 }
