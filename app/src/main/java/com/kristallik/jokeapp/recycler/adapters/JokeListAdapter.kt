881147662c4e49fae39b1f26a1501495715476e7
kristal1ik <korobovtsevaolga@yandex.ru>
2024-11-28 19:29:03 +0300

    feat(JokeListAdapter.kt): companion object

--- app/src/main/java/com/kristallik/jokeapp/recycler/adapters/JokeListAdapter.kt
+++ app/src/main/java/com/kristallik/jokeapp/recycler/adapters/JokeListAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import com.kristallik.jokeapp.data.Joke
+import com.kristallik.jokeapp.data.Source
 import com.kristallik.jokeapp.databinding.JokeItemBinding
 import com.kristallik.jokeapp.databinding.JokeItemNetBinding
 import com.kristallik.jokeapp.recycler.JokeViewHolder
@@ -11,33 +12,46 @@ import com.kristallik.jokeapp.recycler.util.JokeItemCallback
 
 class JokeListAdapter(private val clickListener: (Int) -> Unit) :
     ListAdapter<Joke, JokeViewHolder>(JokeItemCallback()) {
+    companion object {
+        const val TYPE_NETWORK = 1
+        const val TYPE_MANUAL = 2
+        const val TYPE_OTHER = 3
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JokeViewHolder {
-        return if (viewType == 1) {
-            val binding =
-                JokeItemNetBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-            JokeViewHolder.NetworkJokeViewHolder(binding)
-        } else {
-            val binding =
-                JokeItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-            JokeViewHolder.ManualJokeViewHolder(binding)
+        return when (viewType) {
+            TYPE_NETWORK -> {
+                val binding =
+                    JokeItemNetBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                JokeViewHolder.NetworkJokeViewHolder(binding)
+            }
+
+            TYPE_MANUAL -> {
+                val binding =
+                    JokeItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                JokeViewHolder.ManualJokeViewHolder(binding)
+            }
+
+            else -> {
+                throw IllegalStateException("Invalid view type!")
+
+            }
         }
     }
 
     override fun onBindViewHolder(holder: JokeViewHolder, position: Int) {
         val joke = getItem(position)
         holder.bind(joke)
-
         holder.itemView.setOnClickListener {
             clickListener(position)
         }
     }
 
     override fun getItemViewType(position: Int): Int {
-        return if (getItem(position).source == "net") {
-            1
-        } else {
-            0
+        return when (getItem(position).source) {
+            Source.TYPE_NETWORK -> TYPE_NETWORK
+            Source.TYPE_MANUAL -> TYPE_MANUAL
+            else -> TYPE_OTHER
         }
     }
 
