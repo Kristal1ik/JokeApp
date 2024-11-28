7b60f6b8e57fe682d2510839552857047207238b
kristal1ik <korobovtsevaolga@yandex.ru>
2024-11-28 19:32:16 +0300

    feat(JokeListFragment.kt): preferences usage

--- app/src/main/java/com/kristallik/jokeapp/ui/main/MainPresenter.kt
+++ app/src/main/java/com/kristallik/jokeapp/ui/main/MainPresenter.kt
@@ -3,35 +3,51 @@ package com.kristallik.jokeapp.ui.main
 import com.kristallik.jokeapp.data.Joke
 import com.kristallik.jokeapp.data.JokeGenerator
 import com.kristallik.jokeapp.data.JokeListResponse
+import com.kristallik.jokeapp.data.PreferencesProvider
+import com.kristallik.jokeapp.data.Source
 import com.kristallik.jokeapp.network.RetrofitInstance
-import com.kristallik.jokeapp.ui.main.fragment.count
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class MainPresenter(private val view: MainView) {
     private val generator = JokeGenerator
     private var currentPage = 0
     private val jokesPerPage = 10
+    private var count = (view as PreferencesProvider).getJokesCount()
+
 
     suspend fun loadJokes() {
         if (count == 0) {
             generator.jokes = generator.generateJokesData()
             currentPage = 0
+            loadMoreJokes()
         }
-
-        loadMoreJokes()
     }
 
     suspend fun loadMoreJokes() {
         try {
-            val response: JokeListResponse =
-                RetrofitInstance.api.getJokes(amount = jokesPerPage, page = currentPage)
+            val response: JokeListResponse = withContext(Dispatchers.IO) {
+                RetrofitInstance.api.getJokes(
+                    amount = jokesPerPage,
+                    page = currentPage
+                )
+            }
             for (joke in response.jokes) {
-                generator.jokes.add(Joke(joke.id, joke.category, joke.setup, joke.delivery, "net"))
+                generator.jokes.add(
+                    Joke(
+                        joke.id,
+                        joke.category,
+                        joke.setup,
+                        joke.delivery,
+                        Source.TYPE_NETWORK
+                    )
+                )
             }
             if (response.jokes.isEmpty()) {
                 view.showError("No more jokes available!")
             } else {
                 currentPage++
-                count += response.jokes.size
+                (view as PreferencesProvider).setJokesCount(count + response.jokes.size)
                 view.showJokes(generator.jokes)
             }
         } catch (e: Exception) {
