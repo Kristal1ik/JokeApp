6858b19499c3082855cf227bfe46e62a410077aa
kristal1ik <korobovtsevaolga@yandex.ru>
2024-12-02 17:55:21 +0300

    add(build.gradle.kts, Joke.kt): room database

--- app/src/main/java/com/kristallik/jokeapp/data/Joke.kt
+++ app/src/main/java/com/kristallik/jokeapp/data/Joke.kt
@@ -2,9 +2,12 @@ package com.kristallik.jokeapp.data
 
 import android.os.Parcel
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity(tableName = "jokes_saved")
 data class Joke(
-    val id: Int,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val category: String,
     val setup: String,
     val delivery: String,
