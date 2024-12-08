eee4cd77bd51bf07523dd801e02c336e8f645c23
kristal1ik <korobovtsevaolga@yandex.ru>
2024-12-08 18:13:22 +0300

    refactor(presentation): clean architecture

--- app/src/main/java/com/kristallik/jokeapp/ui/add_joke/AddJokeView.kt
+++
@@ -1,7 +0,0 @@
-package com.kristallik.jokeapp.ui.add_joke
-
-interface AddJokeView {
-    fun showCategories(categories: List<String>)
-    fun showError(errorMessage: String)
-    fun saveJoke(message: String)
-}
